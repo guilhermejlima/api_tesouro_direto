682414696f2ccbd1a933d7fc56a39cbfcbee815b
guilherme Jardim <guiljlima@gmail.com>
2019-10-07 20:09:17 -0300

    adicionando o swagger

--- tesouro_direto/settings.py
+++ tesouro_direto/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tesouro_direto'
+    'tesouro_direto',
+    'rest_framework_swagger',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 
--- tesouro_direto/urls.py
+++ tesouro_direto/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='API')
 
 urlpatterns = [
     url(r'^', include('tesouro_direto.tesouro.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', schema_view),
 
 ]
+
+
+
+
+
+
