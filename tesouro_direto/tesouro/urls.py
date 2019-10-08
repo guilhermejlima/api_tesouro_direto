aef81b7f6fbeda35feb136c5ed3474e9ae6216c2
guilherme Jardim <guiljlima@gmail.com>
2019-10-07 23:15:35 -0300

    atualização da api

--- tesouro_direto/tesouro/urls.py
+++ tesouro_direto/tesouro/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from tesouro_direto.tesouro import views
 
 urlpatterns = [
-    url(r'^titulos/$', views.TituloViewSet.as_view(), name='titulos'),
+    url(r'^titulos/$', views.TituloList.as_view(), name='titulo-list'),
+    url(r'^titulo/(?P<pk>[0-9]+)/$', views.TituloDetail.as_view(), name='titulo-detail'),
 
 ]
--- tesouro_direto/tesouro/views.py
+++ tesouro_direto/tesouro/views.py
@@ -1,26 +1,15 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, generics
+from rest_framework import generics
 
 from tesouro_direto.tesouro.models import Titulo
-from tesouro_direto.tesouro.serializers import UserSerializer, GroupSerializer, TituloSerializer
+from tesouro_direto.tesouro.serializers import TituloSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+class TituloList(generics.ListCreateAPIView):
+
+    queryset = Titulo.objects.all()
+    serializer_class = TituloSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-
-class TituloViewSet(generics.ListCreateAPIView):
+class TituloDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Titulo.objects.all()
     serializer_class = TituloSerializer
