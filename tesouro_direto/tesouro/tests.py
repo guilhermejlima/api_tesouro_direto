d40cfd5cbd4d7fa81b96e292b866e91f904de122
guilherme Jardim <guiljlima@gmail.com>
2019-10-07 22:15:14 -0300

    correção erro

--- tesouro_direto/tesouro/tests.py
+++ tesouro_direto/tesouro/tests.py
@@ -1,25 +1,14 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from tesouro_direto.tesouro.models import Titulo
-
 
 class TestApi(APITestCase):
-    def setUp(self):
-        self.nome = "Tesouro IPCA+ 2024"
-        self.vencimento = "15/05/2035"
-        self.taxa_rendimento = "IPCA + 2,61"
-        self.valor_minimo = "R$57,20"
-        self.preco = "R$2.860,49"
-
-        self.titulo = Titulo.objects.create(nome= self.nome, vencimento= self.vencimento, taxa_rendimento= self.taxa_rendimento, valor_minimo= self.valor_minimo, preco= self.preco)
-
-    def test_api_response(self):
+    def test_api_response_success(self):
         response = self.client.get('/titulos/', {})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_api_response(self):
+    def test_api_response_error(self):
         response = self.client.get('/error/', {})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
