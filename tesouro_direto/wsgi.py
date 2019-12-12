ca66fb65b5c4f3e4721c5ed24e94fb88e1ed5f78
guilhermejardim <guiljlima@gmail.com>
2019-12-12 16:25:26 -0300

    configurando secret_key

--- tesouro_direto/wsgi.py
+++ tesouro_direto/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tesouro_direto.settings')
 
+os.environ["SECRET_KEY"] ="19y$+lcx19z19o@y2s5$vw!6^!g@o7j-2#0ojyn_5#elgl)%v@"
+
 application = get_wsgi_application()
